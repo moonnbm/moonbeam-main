6732a2d7e8897fdf60485b1647c9ad2954001ce6
macsint <marius@moonbeam.vet>
2023-01-30 13:46:11 -0700

    refactor(moonbeam-app): #2 - adds splash screens for Android and iOS.
    This PR initializes the moonbeam application for Android, iOS and web, using React Native
    and Expo. In addition, it adds the splash screen for iOS and Android for the app, at initialization/
    startup time.
    
    MOONBM-2

---
+++ packages/moonbeam-app/App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+export default function App() {
+  useEffect(() => {
+    const prepare = async () => {
+      // keep splash screen visible for 1.5 seconds, while we preload anything that we want
+      await SplashScreen.preventAutoHideAsync()
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      await SplashScreen.hideAsync()
+    }
+    prepare()
+  }, [])
+
+  console.log('this is a super cool logging message');
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
